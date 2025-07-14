4a7a4e384e5e7e1ed569b88d193754e59d4a2c2f
Priyanshu1057 <99806982+Priyanshu1057@users.noreply.github.com>
2025-07-14 15:55:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from bot import Bot
 import pyrogram.utils
 
-pyrogram.utils.MIN_CHANNEL_ID = -1001726493524
+pyrogram.utils.MIN_CHANNEL_ID = -1002508703787 -1002774088122 -1002783292697 -1002868541980
 
 if __name__ == "__main__":
     Bot().run()
